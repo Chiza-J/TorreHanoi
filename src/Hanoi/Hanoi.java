bb3781de938bf91645f6c1a30be1150a9e384747
Jose Sanchez <chizaelite1280@gmail.com>
2025-03-04 09:15:57 -0600

    Prueba Link

--- src/Hanoi/Hanoi.java
+++ src/Hanoi/Hanoi.java
@@ -31,7 +31,7 @@ public class Hanoi {
     private void resolverHanoi(int n, int origen, int destino, List<Integer> intermedios) {
         if (n == 0) return;
         if (intermedios.isEmpty()) {
-            moverHanoiClasico(n, origen, destino);
+            moverHanoi(n, origen, destino);
         } else {
             int k = (int) Math.round(n - Math.sqrt(2 * n + 1)) + 1;
             int aux = intermedios.remove(0);
@@ -44,7 +44,7 @@ public class Hanoi {
         }
     }
 
-    private void moverHanoiClasico(int n, int origen, int destino) {
+    private void moverHanoi(int n, int origen, int destino) {
         if (n == 1) {
             if (esMovimientoValido(origen, destino)) {
                 realizarMovimiento(origen, destino);
@@ -52,12 +52,14 @@ public class Hanoi {
             return;
         }
         int aux = obtenerPaloAuxiliar(origen, destino);
-        moverHanoiClasico(n - 1, origen, aux);
+        moverHanoi(n - 1, origen, aux);
         if (esMovimientoValido(origen, destino)) {
             realizarMovimiento(origen, destino);
         }
-        moverHanoiClasico(n - 1, aux, destino);
+        moverHanoi(n - 1, aux, destino);
     }
+    
+    
 
     private boolean esMovimientoValido(int origen, int destino) {
         return !torres[origen].isEmpty() && (torres[destino].isEmpty() || torres[destino].peek() > torres[origen].peek());
